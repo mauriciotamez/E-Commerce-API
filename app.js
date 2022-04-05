5adab3c61830729c8b653385be64ded3a4cda2fe
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-04-05 17:36:25 -0500

    Added helmet and compression to our project.

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 const express = require('express')
+const morgan = require('morgan')
+const compression = require('compression')
+const helmet = require('helmet')
 
 // Controllers
 const { globalErrorHandler } = require('./controllers/error.controller')
 
 // Routers
 const { usersRouter } = require('./routes/users.route')
+const { productsRouter } = require('./routes/products.route')
+const { cartRouter } = require('./routes/cart.routes')
 
 const app = express()
+
+// Middlewares
+app.use(morgan('dev'))
+// compress all responses
+app.use(compression())
+// headers
+app.use(helmet())
 
 // Enable incoming JSON data
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(globalErrorHandler)
 
 // Endpoints
 app.use('/api/v1/users', usersRouter)
-
-app.use(globalErrorHandler)
+app.use('/api/v1/products', productsRouter)
+app.use('/api/v1/cart', cartRouter)
 
 module.exports = { app }
